8aaa69f06f4e16910e90b2827d7983e876d0232b
Jordan Pittman <jordan@cryptica.me>
2021-10-09 20:20:44 -0400

    Tweak build setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,45 @@
-import typescript from "@rollup/plugin-typescript"
+import { defineConfig } from "rollup"
+import del from "rollup-plugin-delete"
 import dts from "rollup-plugin-dts"
 import prettier from "rollup-plugin-prettier"
+import typescript from "@rollup/plugin-typescript"
 
-export default [
+export default defineConfig([
   {
     input: "src/index.ts",
-    output: {
-      dir: "dist",
-      format: "cjs",
-      exports: "named",
-    },
+    output: [
+      {
+        dir: "dist",
+        format: "esm",
+        exports: "named",
+        chunkFileNames: "[name].mjs",
+        entryFileNames: "[name].mjs",
+      },
+      {
+        dir: "dist",
+        format: "cjs",
+        exports: "named",
+        chunkFileNames: "[name].cjs",
+        entryFileNames: "[name].cjs",
+      },
+    ],
     plugins: [
       typescript({
         target: "es2018",
-      })
+      }),
     ],
     external: ["nanocolors", "table", "strip-ansi", "readline"],
   },
   {
     input: "./dist/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [
       dts(),
       prettier(),
+      del({
+        targets: "dist/*.d.ts",
+        hook: "buildEnd",
+      }),
     ],
   },
-]
+])
